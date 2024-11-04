1425119afef24a4ac185608c53999154608fe791
Jordson Galvão <105514196+JordsonZ@users.noreply.github.com>
2024-11-04 17:09:19 +0000

    ajustando paginas de login e cadastro

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,47 +1,63 @@
 import React from 'react';
-import { Button, TextField, Typography, Box, InputAdornment } from '@mui/material';
+import { Button, TextField, Typography, Box, Grid} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined';
+//import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined';
 
 const Cadastro: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Box 
-      display="flex" 
-      flexDirection="column" 
-      alignItems="center" 
-      justifyContent="center" 
-      height="100vh"
-    >
-      <Typography variant="h4" gutterBottom>
-        Cadastro
-      </Typography>
-      <TextField label="Nome" variant="outlined" fullWidth margin="normal" />
-      <TextField label="Email" variant="outlined" fullWidth margin="normal" />
-      <TextField label="Senha" variant="outlined" fullWidth margin="normal" type="password"  InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <KeyOutlinedIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <Button variant="contained" color="primary" fullWidth>
-        Registrar
-      </Button>
+    <Grid container style={{ height: '100vh' }}>
 
-      <Button 
-        variant="text" 
-        color="secondary" 
-        onClick={() => navigate('/login')}
-        style={{ marginTop: '16px' }}
-      >
-        Já tem uma conta? Faça login
-      </Button>
+    <Grid item xs={12} md={6}>
+        <Box
+          sx={{
+            backgroundImage: 'url(/src/image/balb.jfif)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100%',
+          }}
+        />
+      </Grid>
+      
+      <Grid item xs={12} md={6} container direction="column" alignItems="center" justifyContent="center">
+        <Typography variant="h4" gutterBottom>
+          Cadastro
+        </Typography>
+        <TextField label="Nome" variant="outlined" margin="normal" />
+        <TextField label="Email" variant="outlined" margin="normal"  />
+        <TextField label="Senha" variant="outlined" margin="normal" type="password"  />
+        <Button variant="contained" color="primary" >
+          Cadastrar
+        </Button>
+        <Button 
+          variant="text" 
+          color="secondary" 
+          onClick={() => navigate('/Login')}
+          style={{ marginTop: '16px' }}
+          fullWidth
+        >
+          Já tem uma conta? Faça login
+        </Button>
+      </Grid>
 
-    </Box>
+    </Grid>
   );
 };
 
 export default Cadastro;
+
+
+
+// [{
+// 	"resource": "/workspaces/2024-react-ui-libs/src/pages/Login.tsx",
+// 	"owner": "typescript",
+// 	"code": "2339",
+// 	"severity": 8,
+// 	"message": "A propriedade 'vars' não existe no tipo 'Theme'.",
+// 	"source": "ts",
+// 	"startLineNumber": 38,
+// 	"startColumn": 28,
+// 	"endLineNumber": 38,
+// 	"endColumn": 32
+// }]
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
-import { Button, TextField, Typography, Box } from '@mui/material';
+import { Button, TextField, Typography, Box, Grid } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Box 
-      display="flex" 
-      flexDirection="column" 
-      alignItems="center" 
-      justifyContent="center" 
-      height="100vh"
-    >
-      <Typography variant="h4" gutterBottom>
-        Login
-      </Typography>
-      <TextField label="Email" variant="outlined" fullWidth margin="normal" />
-      <TextField label="Senha" variant="outlined" fullWidth margin="normal" type="password" />
-      <Button variant="contained" color="primary" fullWidth>
-        Entrar
-      </Button>
 
-      <Button 
-        variant="text" 
-        color="secondary" 
-        onClick={() => navigate('/cadastro')}
-        style={{ marginTop: '16px' }}
-      >
-        Não tem uma conta? Cadastre-se
-      </Button>
+    <Grid container style={{ height: '100vh' }}>
 
-    </Box>
+    <Grid item xs={12} md={6}>
+        <Box
+          sx={{
+            backgroundImage: 'url(/src/image/balbas.webp)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100%',
+          }}
+        />
+      </Grid>
+
+      <Grid item xs={12} md={6} container direction="column" alignItems="center" justifyContent="center">
+        <Typography variant="h4" gutterBottom>
+          Login
+        </Typography>
+        <TextField label="Email" variant="outlined" margin="normal"  />
+        <TextField label="Senha" variant="outlined" margin="normal" type="password"  />
+        <Button variant="contained" color="primary" >
+          Entrar
+        </Button>
+        <Button 
+          variant="text" 
+          color="secondary" 
+          onClick={() => navigate('/Cadastro')}
+          style={{ marginTop: '16px' }}
+          fullWidth
+        >
+          Não tem uma conta? Cadastre-se
+        </Button>
+      </Grid>
+
+    </Grid>
   );
 };
 
